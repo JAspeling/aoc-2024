907edc7c3076f15bf0a9f115f7c164b15eeec165
Johan Aspeling <johan.aspeling@gmail.com>
2024-12-01 21:40:55 +0100

    day 1

--- src/2024/1/2024-1.spec.ts
+++ src/2024/1/2024-1.spec.ts
@@ -1,10 +1,66 @@
-import { fetchAoc } from '../../utils/fetch-input'
+import {fetchAoc} from '../../utils/fetch-input';
+import {calculateDistance, extractArrays, findSmallest, part1, part2} from './2024-1';
+
+jest.mock('./2024-1.ts', () => ({
+  ...jest.requireActual('./2024-1.ts'),
+  run: jest.fn(),
+}));
+
+  const input = `3   4
+4   3
+2   5
+1   3
+3   9
+3   3`;
 
 describe('fetch 2024', () => {
-  it('should fetch day 1 data', async () =>  {
+  it('should fetch day 1 data', async () => {
     const data = await fetchAoc(2024, 1);
 
     expect(data).toBeTruthy();
     expect(localStorage.getItem('2024-1')).toBeTruthy();
+  });
+
+
+
+  it('should find the distance between 2 points', async () => {
+    expect(calculateDistance(1, 2)).toBe(1);
+  });
+
+  it('should find the minimum value in a list', () => {
+    const list = [5, 2, 7, 5, 1, 8];
+    expect(findSmallest(list)).toBe(1);
+  });
+
+  it('should extract the arrays from the input', () => {
+    const [left, right] = extractArrays(input.split('\n'));
+
+    expect(left).toEqual([3, 4, 2, 1, 3, 3]);
+    expect(right).toEqual([4, 3, 5, 3, 9, 3]);
+  });
+
+  it('should extract the arrays with more than single-digit numbers', () => {
+    const newInput = ["12   32"] 
+    const [left, right] = extractArrays(newInput);
+    
+    expect(left).toEqual([12]);
+    expect(right).toEqual([32]);
   })
-})
+});
+
+describe('part 1', () => {
+  it('should calculate part 1', async () => {
+    const realInput = await fetchAoc(2024, 1)
+    const result = await part1(realInput);
+    expect(result).toBe(2166959);
+  });
+}); 
+
+
+describe('part 2', () => {
+  it('should calculate part 1', async () => {
+    const realInput = await fetchAoc(2024, 1);
+    const result = await part2(realInput);
+    expect(result).toBe(23741109);
+  });
+}); 
--- src/2024/1/2024-1.ts
+++ src/2024/1/2024-1.ts
@@ -1,16 +1,44 @@
-import { timeIt } from '../../utils';
 
-const run = async () => {
-  await timeIt(part1)();
-  await timeIt(part2)();
+export const part1 = async (input: string[]) => {
+  const [leftArr, rightArr] = extractArrays(input);
+  const distances: number[] = [];
+
+  leftArr.sort((a, b) => a - b);
+  rightArr.sort((a, b) => a - b);
+
+  leftArr.forEach((item, index) => {
+    distances.push(calculateDistance(item, rightArr[index]));
+  })
+  return distances.reduce((prev, curr) => prev + curr, 0);
 }
 
-const part1 = async () => {
-  console.log('Part 1:');
+export const part2 = async (input: string[]) => {
+  const [leftArr, rightArr] = extractArrays(input);
+
+  let total = 0
+
+  leftArr.forEach(item => {
+    const count = rightArr.filter(i => i == item).length;
+    total += (item * count)
+  })
+
+  return total;
 }
 
-const part2 = async () => {
-  console.log('Part 2:');
+export const extractArrays = (input: string[]): number[][] => {
+  const leftArray: number[] = [];
+  const rightArray: number[] = [];
+  input.forEach((row: string) => {
+    // Split the row by whitespace
+    const [left, right] = row.split(/\s+/).map(Number);
+
+    leftArray.push(left);
+    rightArray.push(right);   
+  })
+
+  return [leftArray, rightArray]
 }
 
-run();
+export const calculateDistance = (p1: number, p2: number): number => Math.abs(p1 - p2)
+
+export const findSmallest = (list: number[]): number => Math.min(...list);
