6c9d25f569c793931d75fba800f16309cc4af5a5
Johan Aspeling <johan.aspeling@gmail.com>
2024-12-02 23:18:32 +0100

    chore: added day 4

---
+++ src/2024/4/2024-4.spec.ts
@@ -0,0 +1,31 @@
+import {fetchAoc} from '../../utils/fetch-input';
+import {part1, part2} from './2024-4';
+
+const testInput = ``;
+const day = 4;
+
+describe('fetch 2024', () => {
+  it(`should fetch day ${day} data`, async () => {
+    const data = await fetchAoc(2024, day);
+
+    expect(data).toBeTruthy();
+    expect(localStorage.getItem(`2024-${day}`)).toBeTruthy();
+  });
+});
+
+describe('part 1', () => {
+  it('should calculate part 1', async () => {
+    const realInput = await fetchAoc(2024, day)
+    const result = await part1(realInput);
+    // expect(result).toBe(x);
+  });
+});
+
+
+describe('part 2', () => {
+  it('should calculate part 1', async () => {
+    const realInput = await fetchAoc(2024, day);
+    const result = await part2(realInput);
+    // expect(result).toBe(x);
+  });
+});
